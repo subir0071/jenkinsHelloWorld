ae5b807afc0d543b242f7625448585601cdd5faa
Subir_Ghosh <techguy007@gmail.com>
2018-08-01 11:43:54 +0530

    Test jenkins scm poll1

--- axistest/src/axistest/HelloWorld.java
+++ axistest/src/axistest/HelloWorld.java
@@ -3,6 +3,7 @@ package axistest;
 public class HelloWorld {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++)
-			System.out.println(i +" :: Hello World!");
+			System.out.println(i +" :: Hello World!  ");
+		//qq
 	}
 }
